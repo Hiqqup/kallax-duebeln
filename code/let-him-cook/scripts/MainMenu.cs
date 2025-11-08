738952381fe2a3136c46e51e796851a7cebafe99
Max Burckhard <max.burckhard@stud-mail.uni-wuerzburg.de>
2025-11-08 11:30:01 +0100

    Created Main Menu with Fade to Black

---
+++ code/let-him-cook/scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	private void StartGame()
+	{
+		GameManager.Instance.LoadScene("res://scenes/test.tscn");
+	}
+
+	private void ExitGame()
+	{
+		GameManager.Instance.CloseGame();
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
---
+++ code/let-him-cook/scripts/global/PathLookup.cs
@@ -0,0 +1,6 @@
+﻿namespace LetHimCook.scripts.global;
+
+public class PathLookup
+{
+    public const string MainMenuPath = "res://scenes/main_menu.tscn";
+}
