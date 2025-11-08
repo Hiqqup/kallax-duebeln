4c019042d18d9a885e8b6a6f320d19137fd9df9d
Egor Simakov <egor.simakov@stud-mail.uni-wuerzburg.de>
2025-11-08 23:29:00 +0100

    fixed box selection drawing

--- code/let-him-cook/scripts/UnitSelector.cs
+++ code/let-him-cook/scripts/UnitSelector.cs
@@ -1,12 +1,13 @@
 using Godot;
 using System;
 using System.Linq;
+using Vector2 = Godot.Vector2;
 
-public partial class UnitSelector : Control
+public partial class UnitSelector : Node2D
 {
 	private bool _selecting;
 	private Vector2 _dragStart;
-	private Rect2 _selectBox;
+	private Rect2 _selectBox = new Rect2();
 
 	public override void _Input(InputEvent @event)
 	{
@@ -15,29 +16,22 @@ public partial class UnitSelector : Control
 			if (mouseEvent.Pressed)
 			{
 				_selecting = true;
-				//_dragStart = mouseEvent.Position;
 				_dragStart = GetViewport().GetCamera2D().GetGlobalMousePosition();
 			}
 			else
 			{
 				_selecting = false;
-				if (_dragStart.IsEqualApprox(mouseEvent.Position)) 
-					_selectBox = new (mouseEvent.Position, Vector2.Zero);
-				GD.Print("select box: ", _selectBox);
+				if (_dragStart.IsEqualApprox(mouseEvent.Position))
+				{
+					_selectBox.Position = mouseEvent.Position;
+					_selectBox.Size = Vector2.Zero;
+				}
 				UpdateSelectedUnits();
 				QueueRedraw();
 			}
 		} else if (_selecting && @event is InputEventMouseMotion mouseMotion)
 		{
 			var dragEnd = GetViewport().GetCamera2D().GetGlobalMousePosition();
-			//float xMin = Mathf.Min(_dragStart.X, mouseMotion.Position.X);
-			//float yMin = Mathf.Min(_dragStart.Y, mouseMotion.Position.Y);
-			//_selectBox = new Rect2(
-			//	xMin, 
-			//	yMin, 
-			//	Math.Max(_dragStart.X, dragEnd.X) - xMin,
-			//	Math.Max(_dragStart.Y, dragEnd.Y) - yMin);
-			_selectBox = new Rect2();
 			_selectBox.Position = _dragStart;
 			_selectBox.End = dragEnd;
 			
@@ -49,10 +43,8 @@ public partial class UnitSelector : Control
 
 	public override void _Draw()
 	{
-		GD.Print("should draw rect");
 		if (!_selecting)
 		{
-			GD.Print("cant draw rect");
 			return;
 		}
 		DrawRect(_selectBox, new Color("#00ff0066"));
