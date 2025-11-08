5ed3f3fc6639032d7ab76ba6ee48d05c0193363f
Max Burckhard <max.burckhard@stud-mail.uni-wuerzburg.de>
2025-11-08 16:07:00 +0100

    merged

---
+++ code/let-him-cook/scripts/PathPreview.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class PathPreview : Node2D
+{
+	[Export]
+	private Line2D _line;
+	
+	private Vector2 _endPosition;
+
+	public Vector2 EndPosition
+	{
+		get => _endPosition;
+		set
+		{
+			_endPosition = value;
+			UpdateLine();
+		}
+	}
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	private void UpdateLine()
+	{
+		_line.SetPoints(new []{new(0, 0), -(GetParent<Node2D>().Position - EndPosition)});
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
